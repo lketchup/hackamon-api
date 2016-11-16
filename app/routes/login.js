92103cf0ef1e398476c80e843aab9bbac316ef4a
Liam Ketchup <lketchup@thoughtworks.com>
2016-11-16 11:36:14 +1100

    change route

--- app/routes/login.js
+++ app/routes/login.js
@@ -3,7 +3,7 @@ var Student = require('../models/student');
 module.exports = function(app) {
 
     // Return Student with uuid
-    app.get('/students/:uuid', function(req, res) {
+    app.get('/login', function(req, res) {
         var username = req.params.user;
         var password = req.params.pass
         Student.findOne({"username": user, "password": pass}, function(err, data) {
