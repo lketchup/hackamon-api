b787b11c082eaf8f5992b0ecb9b2c802843e2718
Liam Ketchup <lketchup@thoughtworks.com>
2016-11-16 12:32:23 +1100

    added cors header stuff

--- app/routes/login.js
+++ app/routes/login.js
@@ -4,6 +4,8 @@ module.exports = function(app) {
 
     // Return Student with uuid
     app.get('/login', function(req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         var username = req.query.username;
         var password = req.query.password;
         Student.findOne({"username": username, "password": password}, function(err, data) {
