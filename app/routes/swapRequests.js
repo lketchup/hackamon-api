9587592903ce4f7a2af437aa2bbea05e74615a31
darvid7 <bvdarvid@gmail.com>
2016-11-24 23:29:07 +1100

    update trigger url in swapreq/new

--- app/routes/swapRequests.js
+++ app/routes/swapRequests.js
@@ -140,7 +140,7 @@ module.exports = function(app) {
             } else {
                 console.log("Added request"); // + data);
                 // trigger swaps
-                var url = "http://localhost:3000/swaprequest/trigger";
+                var url = "https://reallocateplus.herokuapp.com/swaprequest/trigger";
                 request.post(url, {}, function(err, result, body){
                     if (err) {
                         console.log(err);
