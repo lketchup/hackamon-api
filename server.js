cf3008e27465ff00c15e37764223957ee170df98
darvid7 <bvdarvid@gmail.com>
2016-11-25 00:03:35 +1100

    update cors

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Database
 var mongoose = require('mongoose');
@@ -49,8 +54,3 @@ app.get('/', function(req, res) {
     res.json({"ReAllocatePlus":"API"})
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
